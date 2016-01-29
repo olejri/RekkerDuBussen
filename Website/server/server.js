73d57899edf3c1989b99b691f2967b28c92873a8
Ole Jørgen Rishoff <olejri@gmail.com>
2016-01-29 18:57:22 +0100

    Endre til Google host

--- Website/server/server.js
+++ Website/server/server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server = app.listen(process.env.PORT || 8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
 });
