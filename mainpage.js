18d5e72043eebbdda5d63aae5e0b8212d30c12cc
Ole Jørgen Rishoff <olejri@gmail.com>
2016-01-30 22:59:42 +0100

    Lagt til dropdown og fikset på tekst.

--- mainpage.js
+++ mainpage.js
@@ -1,17 +1,13 @@
 var test;
 $("document").ready(function() {
-
-	/**
-	 * Binding buttons
-	 */
-	$('#button1').button().bind('click', getBuss);
-
+    hentFraRuter();
 });
 
-function getBuss(){
+function hentFraRuter(){
+        var bussid = document.getElementById("valg").value;
 		$.ajax({
 		type: "GET",
-		url: "https://reis.trafikanten.no/reisrest/realtime/getrealtimedata/3010333",
+		url: "https://reis.trafikanten.no/reisrest/realtime/getrealtimedata/"+bussid,
 		dataType: "jsonp",
 		success: function(response) {
 			skriv(response);
@@ -22,68 +18,55 @@ function getBuss(){
 };
 
 function skriv(jsonTest){
-   $("#aboutText").empty();
-   $("#aboutText").append("Tid = " + new Date().toString().split(' ')[4]+ "<br>");
-   $("#aboutText").append("<br> Mot Nydalen <br>");
-   $("#aboutText1").empty();
-   $("#aboutText1").append("Tid = " + new Date().toString().split(' ')[4]+ "<br>");
-   $("#aboutText1").append("<br> Mot Sentrum <br>");
-   for (i = 0; i < jsonTest.length; i++) {
-   var tid = new Date(parseInt(jsonTest[i].ExpectedArrivalTime.substr(6)));
-   var nå = new Date();
-   var diff =  Math.abs(tid-nå)/60000;
-   var avrundet = diff.toFixed(1)
-
-   var tidString = new Date(parseInt(jsonTest[i].ExpectedArrivalTime.substr(6))).toString().split(' ')[4];
-   var destinationName = jsonTest[i].DestinationName;
-     if(destinationName == "Kjelsås stasjon"){
-     var tekst = "Kjelsås stasjon = "+ tidString + " Antall minutter="+ avrundet;
-        if(diff < 1){
-                   $("#aboutText").append("<br> <font color=red>" + tekst + "</font><br>");
-               }
-               if(diff > 1 && diff < 2){
-               $("#aboutText").append("<br> <font color=orange>" + tekst + "</font><br>");
-               }
-               if(diff > 2){
-               $("#aboutText").append("<br> <font color=green>" + tekst + "</font><br>");
-               }
-     }
-     if(destinationName == "Tåsen"){
-     var tekst = "Tåsen = "+ tidString + " Antall minutter="+ avrundet;
-        if(diff < 1){
-            $("#aboutText").append("<br> <font color=red>" + tekst + "</font><br>");
-        }
-        if(diff > 1 && diff < 2){
-        $("#aboutText").append("<br> <font color=orange>" + tekst + "</font><br>");
-        }
-        if(diff > 2){
-        $("#aboutText").append("<br> <font color=green>" + tekst + "</font><br>");
-        }
-     }
-     if(destinationName == "Aker brygge"){
-          var tekst = "Aker brygge = "+ tidString + " Antall minutter="+ avrundet;
-             if(diff < 1){
-                 $("#aboutText1").append("<br> <font color=red>" + tekst + "</font><br>");
-             }
-             if(diff > 1 && diff < 2){
-             $("#aboutText1").append("<br> <font color=orange>" + tekst + "</font><br>");
-             }
-             if(diff > 2){
-             $("#aboutText1").append("<br> <font color=green>" + tekst + "</font><br>");
-             }
-          }
-          if(destinationName == "Ekeberg hageby"){
-               var tekst = "Ekeberg hageby = "+ tidString + " Antall minutter="+ avrundet;
-                  if(diff < 1){
-                      $("#aboutText1").append("<br> <font color=red>" + tekst + "</font><br>");
-                  }
-                  if(diff > 1 && diff < 2){
-                  $("#aboutText1").append("<br> <font color=orange>" + tekst + "</font><br>");
-                  }
-                  if(diff > 2){
-                  $("#aboutText1").append("<br> <font color=green>" + tekst + "</font><br>");
-                  }
-               }
+   $("#venstreDiv").empty();
+   $("#venstreDiv").append("Klokken: " + new Date().toString().split(' ')[4]+ "<br>");
+   $("#høyreDiv").empty();
+   $("#høyreDiv").append("Klokken: " + new Date().toString().split(' ')[4]+ "<br>");
+   var antall = 0;
+   if (jsonTest.length > 30){
+    antall = 30;
+   } else {
+   antall = jsonTest.length;
    }
-   setTimeout(getBuss, 5000);
+
+   for (i = 0; i < antall; i++) {
+       var tid = new Date(parseInt(jsonTest[i].ExpectedArrivalTime.substr(6)));
+       var nå = new Date();
+       var diff =  Math.abs(tid-nå)/60000;
+       var avrundet = diff.toFixed(1)
+       var jsonObj = jsonTest[i];
+       var tidString = new Date(parseInt(jsonObj.ExpectedArrivalTime.substr(6))).toString().split(' ')[4];
+       var holdePlassNummer = jsonObj.DirectionName;
+       var publishedLineName = jsonObj.PublishedLineName;
+       var destinationName = jsonObj.DestinationName;
+
+       leggTilTekst(holdePlassNummer, diff, avrundet, tidString, publishedLineName, destinationName);
+   }
+   setTimeout(hentFraRuter, 5000);
+}
+
+function leggTilTekst(holdePlassNummer, diff, avrundet, tidString, publishedLineName, destinationName){
+    var tekst =  publishedLineName + "-" + destinationName + ". "+ avrundet + " min";
+
+    if(holdePlassNummer == 1) {
+            if(diff < 1){
+                $("#høyreDiv").append("<br> <font color=red>" + tekst + "</font><br>");
+            }
+            if(diff > 1 && diff < 2){
+                $("#høyreDiv").append("<br> <font color=orange>" + tekst + "</font><br>");
+            }
+            if(diff > 2){
+                $("#høyreDiv").append("<br> <font color=green>" + tekst + "</font><br>");
+            }
+    } else {
+            if(diff < 1){
+               $("#venstreDiv").append("<br> <font color=red>" + tekst + "</font><br>");
+            }
+            if(diff > 1 && diff < 2){
+            $("#venstreDiv").append("<br> <font color=orange>" + tekst + "</font><br>");
+            }
+            if(diff > 2){
+               $("#venstreDiv").append("<br> <font color=green>" + tekst + "</font><br>");
+            }
+         }
 }
